d1cdb1e131c4140df1321400f8c1f1bbdf50e800
SumYin <ryanzhang333@gmail.com>
2023-04-18 22:31:27 +0200

    First
    Adding the base files. more to be worked on

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      darkMode: 'class',
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'dark-blue': '#213547',
+      },
+    },
+  }
